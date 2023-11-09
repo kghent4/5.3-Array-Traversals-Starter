eb49a2c5a81fd878826b22711c14653559bcbd0d
Kaylee Ghent <kghent@neisd.net>
2023-11-09 08:29:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions
+
+Modify the algorithms on the 5.3 slides to solve each problem.
+
+1. Find the average of all elements in the array.
+2. Find the minimum of all elements in the array.
+3. Determine if there are any negative numbers in the array.
---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("<- instructions are in README.md!");
+
+        double[] testarr = {4.4, 3.2, 100.0, 8.998, -20.1};
+        double[] testarr2 = {0.3, 400.3, 400.3, 400.3, 20};
+    }
+}
